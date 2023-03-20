1ad76bafef9decb52cdd6a7f694dd8bbb4f07e67
Dev Patel <devpatel379.dp@gmail.com>
2023-03-20 17:00:17 -0400

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,17 @@ import './App.css';
 
 
 function App() {
-  const [state, setState] = useState({message: '', name: ''});
+/*   ;
   const [roomName, setRoomName] = useState('')
   const [userName, setUserName] = useState('')
+  const [chat, setChat] = useState([]); */
+  const [state, setState] = useState({message: '', name: ''})
+  const [roomName, setRoomName] = useState('');
+  const [userName, setUserName] = useState('');
   const [chat, setChat] = useState([]);
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
 
   const socketRef = useRef();
 
@@ -19,28 +26,55 @@ function App() {
   }, []);
 
   useEffect(() => {
-    socketRef.current.on('message', ({name, message}) => {
+    socketRef.current.on('message', ({userName, message}) => {
       console.log('The server has sent some data to all clients');
-      setChat([...chat, {name, message}]);
+      setChat(chat => [...chat, {userName, message}]);
     });
-    socketRef.current.on('user_join', function (data) {
-      setChat([
+    socketRef.current.on('user_join', ({userName, users}) => {
+      setUsers(users)
+      setChat(chat => [
         ...chat,
-        {name: 'ChatBot', message: `${data} has joined the chat`}
+        {name: 'ChatBot', message: `${userName} has joined the chat`}
       ]);
     });
 
+    socketRef.current.on('user_left', ({ userName, users }) => {
+      setUsers(users);
+      setChat(chat => [...chat, { name: 'ChatBot', message: `${userName} left the chat` }]);
+    });
+
+
     return () => {
       socketRef.current.off('message');
-      socketRef.current.off('user-join');
+      socketRef.current.off('user_join');
+      socketRef.current.off('user_left')
     };
   }, [chat]);
 
-  const userjoin = (name) => {
+/*   const userjoin = (name) => {
     socketRef.current.emit('user_join', name);
+  }; */
+
+  const userjoin = e => {
+    e.preventDefault();
+    socketRef.current.emit('user_leave', { roomName, userName });
+    socketRef.current.emit('user_join', { roomName, userName });
   };
 
-  const onMessageSubmit = (e) => {
+  const userleave = () => {
+    socket.emit('leave', { roomName, userName });
+    setMessages([]);
+    setUsers([]);
+  };
+  
+  const handleMessage = e => {
+    e.preventDefault();
+    socket.emit('message', { roomName, userName, message });
+    setMessage('');
+  };
+
+
+/*   const onMessageSubmit = (e) => {
     let msgEle = document.getElementById('message');
     console.log([msgEle.name], msgEle.value);
     setState({...state, [msgEle.name]: msgEle.value});
@@ -52,9 +86,9 @@ function App() {
     setState({message: '', name: state.name});
     msgEle.value = '';
     msgEle.focus();
-  };
+  }; */
 
-  const renderChat = () => {
+/*   const renderChat = () => {
     console.log('In render chat');
     return chat.map(({name, message}, index) => (
       <div key={index}>
@@ -115,6 +149,40 @@ function App() {
       )}
     </div>
   );
+} */
+
+return (
+  <div>
+    {roomName && userName ? (
+      <>
+        <div>
+          <h2>{roomName} Chat Room</h2>
+          <p>Users: {users.join(', ')}</p>
+          <button onClick={handleLeave}>Leave Room</button>
+        </div>
+        <div>
+          {messages.map(({ userName, message }, index) => (
+            <div key={index}>
+              <strong>{userName}: </strong>
+              <span>{message}</span>
+            </div>
+          ))}
+        </div>
+        <form onSubmit={handleMessage}>
+          <input type="text" value={message} onChange={e => setMessage(e.target.value)} />
+          <button type="submit">Send</button>
+        </form>
+      </>
+    ) : (
+      <form onSubmit={handleJoin}>
+        <input type="text" placeholder="Room name" value={roomName} onChange={e => setRoomName(e.target.value)} />
+        <input type="text" placeholder="Username" value={userName} onChange={e => setUserName(e.target.value)} />
+        <button type="submit">Join</button>
+      </form>
+    )}
+  </div>
+);
 }
+
 
 export default App;
