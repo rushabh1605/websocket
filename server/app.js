bcb51f950d526cef8ee47f5786238b83c492cea6
Rushabh Thakkar <62187076+rushabh1605@users.noreply.github.com>
2023-03-21 14:52:41 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,36 +1,28 @@
-const express = require('express')
-const app = express()
+const app = require('express');
 const http = require('http').createServer(app);
 var io = require('socket.io')(http);
-
-const rooms = new Map()
 
 io.on('connection', (socket) => {
   console.log('new client connected', socket.id);
 
-  socket.on('user_join', ({roomName, user_name}) => {
-
-    if(!rooms.has(roomName)) {
-      rooms.set(roomName, new Set())
-    }
-
-    const users = rooms.get(roomName)
-    users.add(user_name)
-    socket.join(roomName)
-    console.log('user_name ' + user_name + 'in room' + roomName);
-    io.to(roomName).emit('user joined', {user_name, users:Array.from(users)})
-    
+  socket.on('join-room', (data) => {
+    socket.leave(data.previousRoom);
+    socket.join(data.newRoom);
+    console.log(data.newRoom);
+    console.log(`joined room ${data.newRoom}`);
   });
 
-  socket.on('message', ({roomName, user_name, message}) => {
-    console.log(user_name, message, socket.id);
-    io.to(roomName).emit('message', {user_name, message});
+  socket.on('user_join', (data) => {
+    console.log('A user joined their name is ' + data.name);
+    io.to(data.room).emit('user_join', data.name);
+  });
+
+  socket.on('message', ({ name, message, room }) => {
+    console.log(name, message, room, socket.id);
+    io.to(room).emit('message', { name, message });
   });
 
   socket.on('disconnect', () => {
-    const users = rooms.get(roomName)
-    users.delete(user_name)
-    socket.leave(roomName)
     console.log('Disconnect Fired');
   });
 });
